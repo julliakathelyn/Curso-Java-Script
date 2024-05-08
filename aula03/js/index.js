7bdf10efec75d1b8f2c02684c166bf1a45c8a0fe
JulliaKathelyn <137806375+julliakathelyn@users.noreply.github.com>
2024-05-08 17:21:57 -0300

    Declarações

---
+++ aula03/js/index.js
@@ -0,0 +1,20 @@
+let a, b, c;
+
+a = 5;
+b = 6; c = 7;
+
+let pessoa = "dimitri";
+
+let pessoas = "dimitris";
+
+function minhafuncao() {
+    document.write(5 + 5);
+    document.write(5 + 5);
+    document.write(5 + 5);
+    document.write(5 + 5);
+    document.write(5 + 5);
+
+}
+
+document.getElementById("texto").innerHTML = 
+"1 aceita quebra de linhas. 2 aceita espaços. 3 nao é necessário finalizar com ponto e virgula mas é recomendado. 4 funciona em bloco de codigo.";
