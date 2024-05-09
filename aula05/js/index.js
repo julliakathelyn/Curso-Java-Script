9cc1a4c150eac83e541c13474a81bb62c9168b7e
JulliaKathelyn <137806375+julliakathelyn@users.noreply.github.com>
2024-05-09 12:06:02 -0300

    Comentarios

---
+++ aula05/js/index.js
@@ -0,0 +1,8 @@
+// Inserindo um texto no paragrafo (comentario para 1 linha apenas)
+
+/*
+tudo que estiver aqui dentro
+sera considerado um comentario (em varias linhas)
+
+*/
+document.getElementById("texto").innerHTML = "Testando comentarios";
