cc60eeb40353f3991378073ce91cf7071e6bebe3
JulliaKathelyn <137806375+julliakathelyn@users.noreply.github.com>
2024-05-09 11:56:23 -0300

    Sintaxe da Linguagem

---
+++ aula04/js/index.js
@@ -0,0 +1,23 @@
+console.log(5+5)
+console.log("5+5")
+console.log(1)
+
+var a = 1;
+var b = 5;
+var c = a+b;
+console.log(c)
+
+var d = a-b;
+console.log(d)
+
+var e = a*b;
+console.log(e)
+
+var f = a/b;
+console.log(f)
+
+var pessoa = "Jullia";
+var PESSOA = "Kathelyn"
+
+console.log(pessoa)
+console.log(PESSOA)
