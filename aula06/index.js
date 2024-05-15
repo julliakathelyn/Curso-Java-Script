25c351dab9235c1b421f4adc2989ff757c490959
JulliaKathelyn <137806375+julliakathelyn@users.noreply.github.com>
2024-05-15 16:03:36 -0300

    var let const

---
+++ aula06/index.js
@@ -0,0 +1,47 @@
+var pote = "Bombom";
+
+alert(pote);
+
+// declaração das variaveis 
+var a, b, c;
+
+//atribuição de valores
+a = 2;
+b = 3;
+c = a + b;
+
+alert(c);
+
+var nome, sobrenome, nomeCompleto, soma;
+
+nome = "Jullia";
+sobrenome = "Kathelyn";
+idade = 20;
+nomeCompleto = nome + " " + sobrenome;
+
+soma = idade + 10;
+pessoa = nome + " " + soma;
+//document.getElementById("texto").innerHTML = pessoa;
+//Jullia 30
+
+//pessoa = idade + 10 + nome;
+//document.getElementById("texto").innerHTML = pessoa;
+//30Jullia
+//document.getElementById("texto").innerHTML = nomeCompleto;
+
+
+//pessoa = nome + idade + 10;
+//document.getElementById("texto").innerHTML = pessoa;
+//Jullia2010
+
+//erro
+//let = cachorro 
+ 
+//cachorro = "Nino";
+//cachorro = "Thor";
+
+//alert(cachorro)
+
+const x = 10;
+
+document.getElementById("texto").innerHTML = x;
